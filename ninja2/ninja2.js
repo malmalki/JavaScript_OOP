35e8c13d2684fe28658ca258460ccff755ae0eca
Mohammed Almalki <m7mdmallki@gmail.com>
2019-10-01 16:02:04 +0300

    ninja2

---
+++ ninja2/ninja2.js
@@ -0,0 +1,32 @@
+function Ninja (name, health){
+
+    this.name = name;
+    this.health = 100;
+
+    var speed = 3;
+    var stringth = 3;
+    
+    this.sayName =  function(){
+        console.log("My ninja name is"+this.name+"!");
+    }
+
+    this.showStats = function(){
+        console.log("Name: "+this.name+", Health: "+this.health+", Speed: "+speed+", Stringth: "+stringth );
+    }
+
+    this.drinkSake = function(){
+        this.health += 10;
+    }
+
+    this.punch = function (enemy){
+        enemy.health -=10
+    } 
+}
+
+var ninja1 = new Ninja("Hyabusa");
+var ninja2 = new Ninja("Nawaf");
+
+
+ninja2.punch(ninja1)
+ninja1.showStats()
+ninja2.showStats();
